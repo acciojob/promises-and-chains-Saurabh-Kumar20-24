130b694e67204449de72e7a9b7b9687bb506c3c4
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-14 15:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let btn=document.getElementById("btn");
+let name=document.getElementById("name");
+let age=document.getElementById("age")
+
+
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	if(name.value=="" || isNaN(age.value)){
+		alert("Please enter valid details")
+	}
+
+		let myPromise= new Promise(function(resolve,reject){
+			setTimeout(()=>{
+				if(age.value>=18){
+					resolve("Welcome, . You can vote.")
+				}else{
+					reject("Oh sorry . You aren't old enough.")
+				}
+			},4000)
+})
+myPromise.then((message)=>{
+	alert(message)
+}).catch((error)=>{
+	alert(error)
+})
+})
+
+
+
+
+
